7722feb9628e5a538898a29509301eb92a2e9fde
Danis96 <danis.preldzic@gmail.com>
2019-11-28 09:54:24 +0100

    Updates

---
+++ precache-manifest.a1b8afd63722db7457a04def6cbbf53a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af02f42c377a99000452705e84e3a74a",
+    "url": "/StarWarsBase/index.html"
+  },
+  {
+    "revision": "70f358a5fcbe51490f17",
+    "url": "/StarWarsBase/static/css/main.bb47a822.chunk.css"
+  },
+  {
+    "revision": "7527538b4b37ec22f0d5",
+    "url": "/StarWarsBase/static/js/2.cb552acc.chunk.js"
+  },
+  {
+    "revision": "70f358a5fcbe51490f17",
+    "url": "/StarWarsBase/static/js/main.2837a70f.chunk.js"
+  },
+  {
+    "revision": "5a194d0a509c4285f428",
+    "url": "/StarWarsBase/static/js/runtime-main.c8784416.js"
+  },
+  {
+    "revision": "792d27d6e943c34348b989bb5eb2a46e",
+    "url": "/StarWarsBase/static/media/ds.792d27d6.jpg"
+  },
+  {
+    "revision": "fa4f9b17743f507832c67a749865a7d3",
+    "url": "/StarWarsBase/static/media/header.fa4f9b17.jpg"
+  },
+  {
+    "revision": "729f115b3f87553e692d8307ec0041fd",
+    "url": "/StarWarsBase/static/media/milfal.729f115b.png"
+  },
+  {
+    "revision": "a43d4efcecf73796e31a754bdf3206cf",
+    "url": "/StarWarsBase/static/media/planet3.a43d4efc.png"
+  },
+  {
+    "revision": "410162e67d39ec00fc6bffdc77592ad5",
+    "url": "/StarWarsBase/static/media/rey.410162e6.png"
+  },
+  {
+    "revision": "9af62aeda1d8be06765e5f78b0429746",
+    "url": "/StarWarsBase/static/media/starlogo1.9af62aed.png"
+  },
+  {
+    "revision": "5d1001864d6c0efe802f0964d8b54325",
+    "url": "/StarWarsBase/static/media/starlogo2.5d100186.png"
+  },
+  {
+    "revision": "ed51d6d130559228c0416a2eaf9f0db8",
+    "url": "/StarWarsBase/static/media/yoda.ed51d6d1.png"
+  }
+]);
